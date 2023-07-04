c0e4251f00bfef242ea6e597681ba8c528b64bf0
hellin <helena.linden@hotmail.se>
2023-07-04 18:06:40 +0200

    Regression starter + solution. Anomaly detection

---
+++ AnomalyDetection/Program.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using PLplot;
+
+var projectDirectory = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "../../../"));
+var assetsRelativePath = Path.Combine(projectDirectory, "assets");
+
+// Set the environment variable for PLplot
+Environment.SetEnvironmentVariable("PLPLOT_LIB", Path.Combine(AppContext.BaseDirectory, "runtimes", "win-x64", "native"));
+
+
+// create the machine learning context
+var context = new MLContext();
+
+// load the data file
+Console.WriteLine("Loading data...");
+var dataView = context.Data.LoadFromTextFile<WeatherData>(path: Path.Combine(assetsRelativePath, "Canadian_climate_history.csv"), hasHeader: true, separatorChar: ',');
+
+// get an array of data points
+var values = context.Data.CreateEnumerable<WeatherData>(dataView, reuseRowObject: false).ToArray();
+
+// Plot data
+//PlotData(values);
+// plot the data
+
+var pl = new PLStream();
+pl.sdev("pngcairo");                // png rendering
+pl.sfnam(Path.Combine(assetsRelativePath, "data.png"));                 // output filename
+pl.spal0("cmap0_alternate.pal");   // alternate color palette
+pl.init();
+pl.env(
+	28000, 28200,                          // x-axis range. Total about 30 000 days.
+	-50, 50,                        // y-axis range
+	AxesScale.Independent,               // scale x and y independently
+	AxisBox.BoxTicksLabelsAxes);             // draw box, ticks, and num ticks
+pl.lab(
+	"Day",                                 // x-axis label
+	"Mean temperature Montreal",              // y-axis label
+	"Mean temperature Montreal over time");   // plot title
+pl.line(
+	(from x in Enumerable.Range(0, values.Count()) select (double)x).ToArray(), 
+	(from p in values select (double)p.MeanTemperatureMontreal).ToArray()
+);
+
+// build a training pipeline for detecting spikes
+var pipeline = context.Transforms.DetectSpikeBySsa(
+	nameof(TemperaturePrediction.Prediction),
+	nameof(WeatherData.MeanTemperatureMontreal),
+	confidence: 99.0,
+	pvalueHistoryLength: 730, // 30
+	trainingWindowSize: 29221, // 90
+	seasonalityWindowSize: 365); // 30
+
+// train the model
+Console.WriteLine("Detecting spikes...");
+var model = pipeline.Fit(dataView);
+
+// predict spikes in the data
+var transformed = model.Transform(dataView);
+var predictions = context.Data.CreateEnumerable<TemperaturePrediction>(transformed, reuseRowObject: false).ToArray();
+
+// find the spikes in the data
+var spikes = (from i in Enumerable.Range(0, predictions.Count())
+	where predictions[i].Prediction[0] == 1
+	select (Day: i, Temperature: values[i].MeanTemperatureMontreal));
+
+var spikesWithDates = (from s in spikes select (Date: values[s.Day].Time, Temperature: s.Temperature)).ToList();
+
+// plot the spikes
+pl.col0(2);     // blue color
+pl.schr(3, 3);  // scale characters
+pl.string2(
+	(from s in spikes select (double)s.Day).ToArray(),
+	(from s in spikes select (double)s.Temperature + 30).ToArray(),
+	"|");
+
+pl.eop();
+
+void PlotData(WeatherData[] values)
+{
+	// plot the data
+	//var pl = new PLStream();
+	//pl.sdev("pngcairo");                // png rendering
+	//pl.sfnam(Path.Combine(assetsRelativePath, "data.png"));                 // output filename
+	//pl.spal0("cmap0_alternate.pal");   // alternate color palette
+	//pl.init();
+	//pl.env(
+	//	29000, 29365,                          // x-axis range. Total about 30 000 days.
+	//	-50,50 ,                        // y-axis range
+	//	AxesScale.Independent,               // scale x and y independently
+	//	AxisBox.BoxTicksLabelsAxes);             // draw box, ticks, and num ticks
+	//pl.lab(
+	//	"Day",                                 // x-axis label
+	//	"Mean temperature Calgary",              // y-axis label
+	//	"Mean temperature Calgary over time");   // plot title
+	//pl.line(
+	//	(from x in Enumerable.Range(0, values.Count()) select (double)x).ToArray(),
+	//	(from p in values select (double)p.MeanTemperatureCalgary).ToArray()
+	//);
+	//pl.eop();
+
+}
+
+/// <summary>
+/// The WeatherData class contains one weather record.
+/// </summary>
+public class WeatherData
+{
+	[LoadColumn(0)] public DateTime Time { get; set; }
+	[LoadColumn(9)] public float MeanTemperatureMontreal { get; set; }
+	[LoadColumn(10)] public float TotalPrecipitationMontreal { get; set; }
+}
+
+/// <summary>
+/// The TemperaturePrediction class contains one temperature spike prediction.
+/// </summary>
+public class TemperaturePrediction
+{
+	[VectorType(3)]
+	public double[] Prediction { get; set; }
+}
+
+
+
---
+++ Regression.Solution/Program.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+
+const string path = "..\\..\\..\\assets\\poverty.csv";
+
+var context = new MLContext(seed: 0);
+
+// Get the data from text file
+var data = context.Data.LoadFromTextFile<Input>(path, hasHeader: true, separatorChar: ',');
+
+// Build the pipeline
+var pipeline = context.Transforms.NormalizeMinMax("PovertyRate")
+	.Append(context.Transforms.Concatenate("Features", "PovertyRate"))
+	.Append(context.Regression.Trainers.Ols());
+
+// Shuffle rows
+data = context.Data.ShuffleRows(data);
+
+// Split into test and train sets
+var testTrainData = context.Data.TrainTestSplit(data, testFraction: 0.2);
+
+// Train the model on train set
+var model = pipeline.Fit(testTrainData.TrainSet);
+
+// Evaluate the model on test set
+var metrics = context.Regression.Evaluate(model.Transform(testTrainData.TestSet));
+
+Console.WriteLine();
+Console.WriteLine($"*************************************************");
+Console.WriteLine($"*       Model quality metrics evaluation         ");
+Console.WriteLine($"*------------------------------------------------");
+Console.WriteLine($"*       RSquared Score:      {metrics.RSquared:0.##}");
+Console.WriteLine($"*       Root Mean Squared Error:      {metrics.RootMeanSquaredError:#.##}");
+
+// Use the model to make a prediction
+var predictor = context.Model.CreatePredictionEngine<Input, Output>(model);
+var input = new Input { PovertyRate = 19.7f };
+var prediction = predictor.Predict(input);
+
+Console.WriteLine($"Predicted birth rate: {prediction.BirthRate:0.##}");
+Console.WriteLine($"Actual birth rate: 58.10");
+Console.WriteLine();
+
+public class Input
+{
+	[LoadColumn(1)]
+	public float PovertyRate;
+
+	[LoadColumn(5), ColumnName("Label")]
+	public float BirthRate;
+}
+
+public class Output
+{
+	[ColumnName("Score")]
+	public float BirthRate;
+}
